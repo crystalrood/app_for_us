23a34001c5bf8d8b0d7aa69cc5536395dde21bbf
Crystal <crystalm@fb.com>
2016-11-04 20:49:01 -0700

    revert to oct 30 local copy

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
 
 });
+
+// Place JavaScript code here...
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
