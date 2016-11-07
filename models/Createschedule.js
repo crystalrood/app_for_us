de2edb9bb01490e98f0cad68b1832b63c0bef749
Crystal <crystalm@fb.com>
2016-11-06 16:56:18 -0800

    testing

--- models/Createschedule.js
+++ models/Createschedule.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const createscheduleSchema = new mongoose.Schema({
-  name: String
-});
+  name: String},
+  { timestamps: true });
 
 const Createschedule = mongoose.model('Createschedule', createscheduleSchema);
+
+
 module.exports = Createschedule;
