8474c8da6c63bfe5ade6a19a7d9c9e633d5e1e31
Crystal <crystalm@fb.com>
2016-11-06 21:18:51 -0800

    testing form to submit to mlab

--- controllers/createschedule.js
+++ controllers/createschedule.js
@@ -13,3 +13,28 @@ exports.getCreateschedule = (req, res) => {
     res.render('createschedule', { createschedule: docs });
   });
 };
+
+exports.postCreateschedule = (req, res, next) => {
+  req.assert('name', 'Name is not valid').len(2);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/createschedule');
+  }
+
+  const createschedule = new Createschedule({
+    name: req.body.name
+  });
+
+
+
+  createschedule.save((err) => {
+    console.log("SAVED!");
+    req.flash('success', { msg: 'This has been saved!' });
+    res.redirect('/createschedule');
+  if(err){
+    console.error(err);
+  }});
+};
