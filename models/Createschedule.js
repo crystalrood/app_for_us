dfe5cf83d4b64ec18e02952688a543353f5ebe44
Erik Rood <erikrood@eriks-air.attlocal.net>
2016-11-23 18:03:08 -0800

    adding user id again again again again again

--- models/Createschedule.js
+++ models/Createschedule.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const createscheduleSchema = new mongoose.Schema({
   {user_id: String},
   {name: String}
-}
+},
   { timestamps: true });
 
 const Createschedule = mongoose.model('Createschedule', createscheduleSchema);
