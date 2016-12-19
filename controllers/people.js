fc75371b501fef308343e946645265681630b28d
Crystal <crystalm@fb.com>
2016-12-18 19:40:30 -0800

    trying to get token to work for ajax

--- controllers/people.js
+++ controllers/people.js
@@ -126,10 +126,41 @@ exports.postShift = (req, res, next) => {
 };
 
 
-exports.postUpdateShift = (req, res) => {
-  res.render('/people', {
-    title: 'Account Management'
+exports.postUpdateShift = (req, res, next) => {
+  req.assert('name', 'Name is not valid').len(2);
+  req.assert('type', 'Please ensure employee type or role is correct').len(2);
+  req.assert('email', 'Please enter a valid email address.').isEmail();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/people');
+  }
+  /* define what needs to be saved*/
+  const people = new People({
+    userid: req.user.id,
+    name: req.body.name,
+    type: req.body.type,
+    min_hours: req.body.min_hours,
+    max_hours: req.body.max_hours,
+    email: req.body.email,
+    phone_number:req.body.phone_number
   });
+
+  people.save((err) => {
+    /* this provides a block if the error is that hte email address is already associated with an employee*/
+    if (err) {
+      if (err.code === 11000) {
+        req.flash('errors', { msg: 'The email address you have entered is already associated with an account.' });
+        return res.redirect('/people');
+      }
+      return next(err);
+    }
+    console.log("SAVED!");
+    req.flash('success', { msg: 'This has been saved!' });
+    res.redirect('/people');
+});
 };
 
 exports.getUpdateShift = (req, res) => {
