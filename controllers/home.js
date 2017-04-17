1d75d904590482b850a32c6b3a5dda4b2570a7f8
Crystal Wesnoski <crystalm@fb.com>
2017-04-16 17:28:44 -0700

    added dropdown that works for manager's stuff, now need to add conditions to fill out tables

--- controllers/home.js
+++ controllers/home.js
@@ -6,7 +6,7 @@
  const async = require('async');
  const passport = require('passport');
  const Modeledshift = require('../models/Modeledshift.js');
-
+ const Quickshifts_customer_timeline = require('../models/Quickshifts_customer_timeline.js');
 
 
 
@@ -15,14 +15,55 @@ exports.index = (req, res, next) => {
   //find it if it's a manager
    try{
     if (req.user.user_type == 'schedule_type') {
-      Modeledshift.find({manager_userid: req.user.id},
-      function (err, docs) {
-        if (err) return handleError(err);
-        res.render('home', { docs: docs });
-      });
+
+
+          var locals = {};
+          var tasks = [
+              function(callback){
+
+                Modeledshift.find({manager_userid: req.user.id}, function (err, docs) {
+                  if (err) { return callback(err); }
+                  if (docs != null){
+                    locals.modeledshifts = docs;
+                    callback();
+                  }
+                  else{
+                    locals.modeledshifts = docs;
+                    callback();
+                  }
+                });
+              },
+
+
+              function(callback){
+                  Quickshifts_customer_timeline.find({manager_userid: req.user.id}, function (err, docs1) {
+                  if (err) { return next(err); }
+                  if (docs1 != null){
+                    locals.schedule = docs1;
+                    callback();
+                  }
+                  else{
+                    locals.schedule = docs1;
+                    callback();
+                  }
+
+                });
+              },
+
+
+
+          ];
+
+        async.parallel(tasks, function(err) {
+            if (err) return next(err);
+            console.log(locals)
+            res.render('home', locals);
+        });
 
 
     }
+
+
 
   //find if it's an employees
    if (req.user.user_type == 'employee_type') {
@@ -34,6 +75,7 @@ exports.index = (req, res, next) => {
     }
   }catch(e){
     if(e){
+      console.log(e)
       res.render('home', {
         title: 'Home'
       });
